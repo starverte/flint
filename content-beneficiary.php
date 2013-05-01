e5150d6d24942b914f6abf7d67aec63ef2125c98
Matt Beall <mbeall@starverte.com>
2013-05-01 01:50:23 -0600

    Update content-beneficiary.php
    Added support for bootstrap fluid columns and bootstrap icons for meta data

--- content-beneficiary.php
+++ content-beneficiary.php
@@ -5,30 +5,35 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title">
-			<?php if (is_archive()) { ?>
-				 <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'flint' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-			<?php } else {
-				the_title();
-			}?></h1>
-
-		<div class="entry-meta">
-			<?php cause_goal(); ?>
-            <div class="progress progress-striped active">
-            <?php $percent = cause_raised( array( 'before' => '', 'after' => '' ) , 'percent');
-			echo '<div class="bar" style="width: ' . $percent . ';"></div>';
-			?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php flint_link_pages( array( 'before' => '<div class="pagination"><ul>', 'after' => '</ul></div>' ) ); ?>
-	</div><!-- .entry-content -->
+<article id="post-<?php the_ID(); ?>" <?php post_class('container-fluid'); ?>>
+    <div class="row-fluid">
+        <div class="span4">
+        	<?php the_post_thumbnail('large'); ?>
+        </div><!-- .span4 -->
+        <div class="span8">
+            <header class="entry-header">
+                <h1 class="entry-title">
+                    <?php if (is_archive()) { ?>
+                         <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'flint' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+                    <?php } else {
+                        the_title();
+                    }?></h1>
+        
+                <div class="entry-meta">
+                    <?php cause_gender('<div class="cause-gender"><i class="icon-user icon-white"></i> ','</div>'); ?>
+                    <?php cause_age('<div class="cause-age"><i class="icon-time icon-white"></i> ','</div>'); ?>
+                </div><!-- .entry-meta -->
+            </header><!-- .entry-header -->
+        
+            <div class="entry-content">
+                <?php the_content(); ?>
+		<a class="btn btn-danger" href="#" style="color:#fff;float:right;">Donate</a>
+                <?php flint_link_pages( array( 'before' => '<div class="pagination"><ul>', 'after' => '</ul></div>' ) ); ?>
+            </div><!-- .entry-content -->
 
 	<footer class="entry-meta">
 		<?php edit_post_link( __( 'Edit', 'flint' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
+        </div><!-- .span8 -->
+    </div><!-- .row-fluid -->
 </article><!-- #post-<?php the_ID(); ?> -->
