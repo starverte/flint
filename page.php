f6777c7f10086a806eab748d35ef66e63448dbaa
Matt Beall <mbeall@starverte.com>
2013-05-05 17:53:01 -0600

    Update page.php
    Change from golden grid to bootstrap styles

--- page.php
+++ page.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-		<div id="primary" class="content-area">
+		<div id="primary" class="content-area span9">
 			<div id="content" class="site-content" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
