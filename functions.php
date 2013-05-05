752a58d775e904da8c6efd6984c1790b007dc3e2
Matt Beall <github@starverte.com>
2013-05-05 16:16:08 -0600

    Add Font Awesome and move bootstrap into same directory

--- functions.php
+++ functions.php
@@ -109,12 +109,15 @@ function flint_scripts() {
 	if (is_plugin_inactive('steel/steel.php')) {
 		// Load scripts and styles for Twitter Bootstrap
 		wp_register_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js' , array('jquery') , '2.3.1', true );
-		wp_register_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css' );
+		wp_register_style( 'bootstrap-style', get_template_directory_uri() . '/inc/css/bootstrap.min.css' );
 		wp_register_script( 'bootstrap-run', get_template_directory_uri() . '/js/run.js' , array('bootstrap') , '' , true );
 		wp_enqueue_script( 'bootstrap' );
 		wp_enqueue_style( 'bootstrap-style' );
 		wp_enqueue_script( 'bootstrap-run' );
 	}
+
+	wp_register_style( 'font-awesome', get_template_directory_uri() . '/inc/css/font-awesome.min.css' );
+	wp_enqueue_style( 'font-awesome' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
