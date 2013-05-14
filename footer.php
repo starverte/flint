e5fca6c295a709f2ce173b3e7b0adfe2394512f9
Matt Beall <mbeall@starverte.com>
2013-05-13 21:17:47 -0500

    Reset footer.php

--- footer.php
+++ footer.php
@@ -5,21 +5,20 @@
  * Contains the closing of the id=main div and all content after
  *
  * @package Flint
- * @since Flint 1.0
  */
 ?>
 
-	</div><!-- #main .site-main -->
+	</div><!-- #main -->
 
-	<footer id="colophon" class="site-footer wrapper" role="contentinfo">
+	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'flint_credits' ); ?>
-			Proudly powered by <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'flint' ); ?>">WordPress</a> and the <a href="http://sparks.starverte.com/" title="<?php esc_attr_e( 'A WordPress Development Framework', 'flint' ); ?>">Sparks Framework</a>
+			Proudly powered by <a href="http://wordpress.org/" rel="generator" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'flint' ); ?>">WordPress</a> and the <a href="http://sparks.starverte.com/" rel="generator" title="<?php esc_attr_e( 'A WordPress Development Framework', 'flint' ); ?>">Sparks Framework</a>
 			<span class="sep"> | </span>
 			<?php $theme = wp_get_theme(); printf( __( 'Theme: %1$s by %2$s.', 'flint' ), $theme, '<a href="'.$theme->{'Author URI'}.'" rel="designer">'.$theme->{'Author'}.'</a>' ); ?>
 		</div><!-- .site-info -->
-	</footer><!-- #colophon .site-footer -->
-</div><!-- #page .hfeed .site -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
