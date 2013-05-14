236653af2d0fd7a0339ac4272d6b9c7c7fed010f
Matt Beall <mbeall@starverte.com>
2013-05-14 18:49:57 -0500

    Add bootstrap edit link

--- content-single.php
+++ content-single.php
@@ -4,6 +4,13 @@
  */
 ?>
 
+<?php if ( is_user_logged_in() ) { ?>
+	<div class="container-fluid">
+		<div class="row-fluid">
+                	<a class="btn btn-info btn-small" href="<?php echo get_edit_post_link(); ?>" style="color:#fff;float:right;"><i class="icon-edit icon-white"></i> Edit</a>
+		</div>
+	</div>
+<?php } ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -58,6 +65,5 @@
 			);
 		?>
 
-		<?php edit_post_link( __( 'Edit', 'flint' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
