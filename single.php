1f8a5d21c36167429ed133ce015b7ed0a79c70c2
Matt Beall <mbeall@starverte.com>
2013-07-28 17:24:18 -0600

    Update single.php for Boostrap 3 RC 1

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area span9">
+	<div id="primary" class="content-area col-lg-9">
 		<div id="content" class="site-content" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
