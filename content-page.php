e7bfff31a85e0e3f1f796a7dd666d6c776ac7faa
Matt Beall <mbeall@starverte.com>
2013-08-01 23:01:34 -0600

    Update featured image on content-page.php

--- content-page.php
+++ content-page.php
@@ -11,7 +11,7 @@
   	<div class="row"><a class="btn btn-default btn-small" href="<?php echo get_edit_post_link(); ?>" style="float:right;">Edit</a></div>
 <?php } ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  	<div class="<?php if (featured_image_vertical()) { echo 'col-lg-2 col-sm-2'; } ?> featured-image"><?php the_post_thumbnail(); ?></div>
+  	<div class="<?php if (featured_image_vertical()) { echo 'col-lg-2 col-sm-2 hidden-sm featured-vertical'; } else {echo 'featured-horizontal';} ?>"><?php the_post_thumbnail(); ?></div>
   	<?php if (featured_image_vertical()) { ?><div class="col-lg-10 col-sm-10" id="content-container"><?php } ?>
       <header class="entry-header">
         <h1 class="entry-title"><?php if (is_single()) { echo the_title(); } else { $permalink = get_permalink(); $title = get_the_title(); echo '<a href="' . $permalink .'" rel="bookmark">' . $title . '</a>'; } ?></h1>
