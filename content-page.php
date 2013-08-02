3c1b40170b4fbde4e6f92298716065c3a9702785
Matt Beall <mbeall@starverte.com>
2013-08-01 22:24:24 -0600

    Update pagination and edit button and add featured image

--- content-page.php
+++ content-page.php
@@ -6,25 +6,25 @@
  */
 ?>
 
-<?php if ( current_user_can('edit_pages') ) { ?>
-	<div class="container">
-		<div class="row">
-                	<a class="btn btn-small" href="<?php echo get_edit_post_link(); ?>" style="float:right;">Edit</a>
-		</div>
-	</div>
-<?php } ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			flint_link_pages( array(
-				'before' => '<div class="pagination"><ul>',
-				'after'  => '</ul></div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+<?php if ( current_user_can('edit_pages') ) { ?>
+  	<div class="row"><a class="btn btn-default btn-small" href="<?php echo get_edit_post_link(); ?>" style="float:right;">Edit</a></div>
+<?php } ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  	<div class="<?php if (featured_image_vertical()) { echo 'col-lg-2 col-sm-2'; } ?> featured-image"><?php the_post_thumbnail(); ?></div>
+  	<?php if (featured_image_vertical()) { ?><div class="col-lg-10 col-sm-10" id="content-container"><?php } ?>
+      <header class="entry-header">
+        <h1 class="entry-title"><?php if (is_single()) { echo the_title(); } else { $permalink = get_permalink(); $title = get_the_title(); echo '<a href="' . $permalink .'" rel="bookmark">' . $title . '</a>'; } ?></h1>
+      </header><!-- .entry-header -->
+      
+      <div class="entry-content">
+        <?php the_content(); ?>
+        <?php
+          flint_link_pages( array(
+            'before' => '<ul class="pagination">',
+            'after'  => '</ul>',
+          ) );
+        ?>
+      </div><!-- .entry-content -->
+     <?php if (featured_image_vertical()) { ?></div><?php } ?>
 </article><!-- #post-<?php the_ID(); ?> -->
