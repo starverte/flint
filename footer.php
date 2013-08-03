9b211928fd756b4a0e58a0d12495c206c488ea7f
Matt Beall <mbeall@starverte.com>
2013-08-02 22:57:35 -0600

    Remove "and the Sparks Framework" and rel="" to satisfy WordPress and W3C

--- footer.php
+++ footer.php
@@ -13,9 +13,9 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'flint_credits' ); ?>
-			Proudly powered by <a href="http://wordpress.org/" rel="generator" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'flint' ); ?>">WordPress</a> and the <a href="http://sparks.starverte.com/" rel="generator" title="<?php esc_attr_e( 'A WordPress Development Framework', 'flint' ); ?>">Sparks Framework</a>
+			Proudly powered by <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'flint' ); ?>">WordPress</a>
 			<span class="sep"> | </span>
-			<?php $theme = wp_get_theme(); printf( __( 'Theme: %1$s by %2$s.', 'flint' ), $theme, '<a href="'.$theme->{'Author URI'}.'" rel="designer">'.$theme->{'Author'}.'</a>' ); ?>
+			<?php $theme = wp_get_theme(); printf( __( 'Theme: %1$s by %2$s.', 'flint' ), $theme, '<a href="'.$theme->{'Author URI'}.'">'.$theme->{'Author'}.'</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
