465b00c23f582c78dd09a10bffe5b3563b8cc778
Matt Beall <mbeall@starverte.com>
2013-08-12 01:16:54 -0600

    Update flint_posted_on

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -120,11 +120,15 @@ if ( ! function_exists( 'flint_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function flint_posted_on() {
-  printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'flint' ),
+  if (get_the_date('Y') != date('Y')) :
+    $postdate = esc_html( get_the_date('F j, Y') );
+  else :
+    $postdate = esc_html( get_the_date('F j') );
+  endif;
+  printf( __( 'Posted on <a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s">%3$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s" rel="author">%6$s</a></span></span>', 'flint' ),
     esc_url( get_permalink() ),
-    esc_attr( get_the_time() ),
     esc_attr( get_the_date( 'c' ) ),
-    esc_html( get_the_date() ),
+    $postdate,
     esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
     esc_attr( sprintf( __( 'View all posts by %s', 'flint' ), get_the_author() ) ),
     get_the_author()
