aa7d115fe7ac04bff8f9733f65d67a24c1cfa4ee
Matt Beall <mbeall@starverte.com>
2013-08-11 20:13:23 -0600

    Update versioning

--- functions.php
+++ functions.php
@@ -128,20 +128,20 @@ add_action( 'widgets_init', 'flint_widgets_init' );
 function flint_scripts() {
   
   // Load Twitter Bootstrap
-  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.0.0-rc1', true );
-  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array() , '3.0.0-rc1' );
+  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '8bdffec', true );
+  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array() , '8bdffec' );
   
   //Load Font Awesome
-  wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.css', array('bootstrap-css'), '3.2.1' );
+  wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '3.2.1' );
 
-  wp_enqueue_script( 'flint-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+  wp_enqueue_script( 'flint-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '9f3e2cd', true );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
   }
 
   if ( is_singular() && wp_attachment_is_image() ) {
-    wp_enqueue_script( 'flint-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+    wp_enqueue_script( 'flint-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '4c99b2a' );
   }
   
   //Load Google Font 'Open Sans'
@@ -153,7 +153,7 @@ function flint_scripts() {
 add_action( 'wp_enqueue_scripts', 'flint_scripts' );
 
 function flint_admin_scripts() {
-  wp_enqueue_style( 'flint-admin', get_template_directory_uri() . '/css/flint-admin.css', array(), '1.0.0-rc3' );
+  wp_enqueue_style( 'flint-admin', get_template_directory_uri() . '/css/flint-admin.css', array(), theme_version() );
 }
 add_action( 'admin_enqueue_scripts', 'flint_admin_scripts' );
 
