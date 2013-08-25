0726e667299bcadf3770a8a4c0215757edf955d8
Matt Beall <mbeall@starverte.com>
2013-08-24 23:02:47 -0600

    Update version of Bootstrap

--- functions.php
+++ functions.php
@@ -128,8 +128,8 @@ add_action( 'widgets_init', 'flint_widgets_init' );
 function flint_scripts() {
   
   // Load Twitter Bootstrap
-  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '237640c', true );
-  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array() , 'c79fa6a' );
+  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.0.0', true );
+  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array() , '3.0.0' );
 
   wp_enqueue_script( 'flint-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '9f3e2cd', true );
 
