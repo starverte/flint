67b49e7441c140073c8f8e17d2801a533dfddd10
Matt Beall <mbeall@starverte.com>
2013-10-09 01:15:23 -0600

    Split footer.php into multiple templates

---
+++ footer-close.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing elements
+ *
+ * @package Flint
+ */
+?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
