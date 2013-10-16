f2820f2b4a7edcf215fd1276acb030d9035e13ef
Matt Beall <mbeall@starverte.com>
2013-10-15 23:17:18 -0600

    Add flint_font_css to header-head.php

--- header-head.php
+++ header-head.php
@@ -20,6 +20,7 @@
   <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
   <![endif]-->
   <?php wp_head(); ?>
+  <?php flint_font_css(); ?>
 </head>
 <body <?php if ( is_page_template('template-clear.php') ) { body_class('clear'); } else { body_class(); } ?>>
   <?php if ( function_exists( 'steel_open' ) ) { steel_open(); } ?>
