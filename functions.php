e1d1a6e135eb0bba575a908db888fcc5a8466182
Matt Beall <mbeall@starverte.com>
2013-10-17 22:33:40 -0600

    Change name to flint_options_css and add darkenHex and lightenHex functions

--- functions.php
+++ functions.php
@@ -330,10 +330,11 @@ function flint_custom_footer() {
   echo '</div>';
 }
 
-function flint_font_css() {
-	$options = get_option( 'flint_general' );
+function flint_options_css() {
+	$general = get_option( 'flint_general' );
+	$colors = get_option( 'flint_colors' );
 	echo '<style type="text/css">'; 
-	switch ($options['font']) {
+	switch ($general['font']) {
 		case 'open-sans':
 			echo 'body { font-family: "Open Sans", sans-serif; font-weight: 300; }';
 			echo 'h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 { font-family: "Open Sans", sans-serif; font-weight: 600}';
@@ -360,6 +361,14 @@ function flint_font_css() {
 			echo 'b, strong { font-weight: 700; }';
 			break;
 	}
+	$canvas_alt = darkenHex($colors['canvas'],10);
+	echo '.navbar-inverse, #masthead, #colophon { background: ' . $colors['canvas'] . '; color: ' . $colors['canvas-text'] . '; }';
+	
+	echo '.navbar-inverse .navbar-nav > li > a, #colophon a { color: ' . $colors['canvas-link'] . '; }';
+	echo '.navbar-inverse .navbar-nav > .dropdown > a .caret { border-top-color: ' . $colors['canvas-link'] . '; border-bottom-color: ' . $colors['canvas-link'] . '; }';
+	if ($colors['canvas'] != '#222222') { echo '.navbar-inverse .navbar-nav > .open > a, .navbar-inverse .navbar-nav > .open > a:hover, .navbar-inverse .navbar-nav > .open > a:focus { color: #fff; background-color: ' . $canvas_alt . ';
+}';
+echo '.navbar-inverse { border-color: ' . $canvas_alt . ';}'; }
 	echo '</style>';
 }
 
@@ -384,4 +393,152 @@ function flint_font_load() {
 			break;
 	}
 	echo '</style>';
+}
+
+/**
+ * Converts Hex to HSL
+ */
+function HexHSL( $HexColor ) {
+  $HexColor    = str_replace( '#', '', $HexColor );
+  
+  if( strlen( $HexColor ) < 3 ) str_pad( $HexColor, 3 - strlen( $HexColor ), '0' );
+  
+  $Add         = strlen( $HexColor ) == 6 ? 2 : 1;
+  $AA          = 0;
+  $AddOn       = $Add == 1 ? ( $AA = 16 - 1 ) + 1 : 1;
+  
+  $Red         = round( ( hexdec( substr( $HexColor, 0, $Add ) ) * $AddOn + $AA ) / 255, 6 );
+  $Green       = round( ( hexdec( substr( $HexColor, $Add, $Add ) ) * $AddOn + $AA ) / 255, 6 );
+  $Blue        = round( ( hexdec( substr( $HexColor, ( $Add + $Add ) , $Add ) ) * $AddOn + $AA ) / 255, 6 );
+  
+  
+  $HSLColor    = array( 'Hue' => 0, 'Saturation' => 0, 'Luminance' => 0 );
+  
+  $Minimum     = min( $Red, $Green, $Blue );
+  $Maximum     = max( $Red, $Green, $Blue );
+  
+  $Chroma      = $Maximum - $Minimum;
+  
+  $HSLColor['Luminance'] = ( $Minimum + $Maximum ) / 2;
+  
+  if( $Chroma == 0 ) {
+    $HSLColor['Luminance'] = round( $HSLColor['Luminance'], 3 );
+    return $HSLColor;
+  }
+  
+  $Range = $Chroma * 6;
+  
+  $HSLColor['Saturation'] = $HSLColor['Luminance'] <= 0.5 ? $Chroma / ( $HSLColor['Luminance'] * 2 ) : $Chroma / ( 2 - ( $HSLColor['Luminance'] * 2 ) );
+  
+  if( $Red <= 0.004 || $Green <= 0.004 || $Blue <= 0.004 )
+    $HSLColor['Saturation'] = 1;
+  
+  if( $Maximum == $Red ) { $HSLColor['Hue'] = round( ( $Blue > $Green ? 1 - ( abs( $Green - $Blue ) / $Range ) : ( $Green - $Blue ) / $Range ), 3 ); }
+  else if( $Maximum == $Green ) { $HSLColor['Hue'] = round( ( $Red > $Blue ? abs( 1 - ( 4 / 3 ) + ( abs ( $Blue - $Red ) / $Range ) ) : ( 1 / 3 ) + ( $Blue - $Red ) / $Range ), 3 ); }
+  else { $HSLColor['Hue'] = round( ( $Green < $Red ? 1 - 2 / 3 + abs( $Red - $Green ) / $Range : 2 / 3 + ( $Red - $Green ) / $Range ), 3 ); }
+  
+  $HSLColor['Saturation'] = round( $HSLColor['Saturation'], 3 );
+  $HSLColor['Luminance']  = round( $HSLColor['Luminance'], 3 );
+  
+  return $HSLColor;
+}
+
+/**
+ * Converts HSL to Hex
+ */
+function HSLHex( $Hue = 0, $Saturation = 0, $Luminance = 0 ) {
+  
+  $HSLColor    = array( 'Hue' => $Hue, 'Saturation' => $Saturation, 'Luminance' => $Luminance );
+  $RGBColor    = array( 'Red' => 0, 'Green' => 0, 'Blue' => 0 );
+  
+  
+  foreach( $HSLColor as $Name => $Value ) {
+    if( is_string( $Value ) && strpos( $Value, '%' ) !== false )
+      $Value = round( round( (int)str_replace( '%', '', $Value ) / 100, 2 ) * 255, 0 );
+    
+    else if( is_float( $Value ) )
+      $Value = round( $Value * 255, 0 );
+    
+    $Value    = (int)$Value * 1;
+    $Value    = $Value > 255 ? 255 : ( $Value < 0 ? 0 : $Value );
+    $ValuePct = round( $Value / 255, 6 );
+    
+    define( "{$Name}", $ValuePct );
+  }
+  
+  $RGBColor['Red']   = Luminance;
+  $RGBColor['Green'] = Luminance;
+  $RGBColor['Blue']  = Luminance;
+  
+  $Radial  = Luminance <= 0.5 ? Luminance * ( 1.0 + Saturation ) : Luminance + Saturation - ( Luminance * Saturation );
+  
+  if( $Radial > 0 ) {
+    $Ma   = Luminance + ( Luminance - $Radial );
+    $Sv   = round( ( $Radial - $Ma ) / $Radial, 6 );
+    $Th   = Hue * 6;
+    $Wg   = floor( $Th );
+    $Fr   = $Th - $Wg;
+    $Vs   = $Radial * $Sv * $Fr;
+    $Mb   = $Ma + $Vs;
+    $Mc   = $Radial - $Vs;
+    
+    if ($Wg == 1) {
+      $RGBColor['Red']   = $Mc;
+      $RGBColor['Green'] = $Radial;
+      $RGBColor['Blue']  = $Ma;
+    }
+    else if( $Wg == 2 ) {
+      $RGBColor['Red']   = $Ma;
+      $RGBColor['Green'] = $Radial;
+      $RGBColor['Blue']  = $Mb;
+    }
+    else if( $Wg == 3 ) {
+      $RGBColor['Red']   = $Ma;
+      $RGBColor['Green'] = $Mc;
+      $RGBColor['Blue']  = $Radial;
+    }
+    else if( $Wg == 4 ) {
+      $RGBColor['Red']   = $Mb;
+      $RGBColor['Green'] = $Ma;
+      $RGBColor['Blue']  = $Radial;
+    }
+    else if( $Wg == 5 ) {
+      $RGBColor['Red']   = $Radial;
+      $RGBColor['Green'] = $Ma;
+      $RGBColor['Blue']  = $Mc;
+    }
+    else {
+      $RGBColor['Red']   = $Radial;
+      $RGBColor['Green'] = $Mb;
+      $RGBColor['Blue']  = $Ma;
+    }
+  }
+  
+  $RGBColor['Red']   = ($C = round( $RGBColor['Red'] * 255, 0 )) < 15 ? '0'.dechex( $C ) : dechex( $C );
+  $RGBColor['Green'] = ($C = round( $RGBColor['Green'] * 255, 0 )) < 15 ? '0'.dechex( $C ) : dechex( $C );
+  $RGBColor['Blue']  = ($C = round( $RGBColor['Blue'] * 255, 0 )) < 15 ? '0'.dechex( $C ) : dechex( $C );
+  
+  return $RGBColor;
+}
+
+/**
+ * Darkens Hex color by defined percentage
+ */
+function darkenHex( $HexColor, $percent ) {
+  $HSLColor = HexHSL($HexColor);
+  $HSLColor['Luminance'] = $HSLColor['Luminance'] - ($percent/100);
+  $HSLColor['Luminance'] = $HSLColor['Luminance'] < 0 ? 0 : $HSLColor['Luminance'];
+  $RGBColor = HSLHex($HSLColor['Hue'],$HSLColor['Saturation'],$HSLColor['Luminance']);
+  return '#' . $RGBColor['Red'].$RGBColor['Green'].$RGBColor['Blue'];
+}
+
+/**
+ * Lightens Hex color by defined percentage
+ */
+function lightenHex( $HexColor, $percent ) {
+  $HSLColor = HexHSL($HexColor);
+  $HSLColor['Luminance'] = $HSLColor['Luminance'] + ($percent/100);
+  $HSLColor['Luminance'] = $HSLColor['Luminance'] < 0 ? 0 : $HSLColor['Luminance'];
+  $RGBColor = HSLHex($HSLColor['Hue'],$HSLColor['Saturation'],$HSLColor['Luminance']);
+  return '#' . $RGBColor['Red'].$RGBColor['Green'].$RGBColor['Blue'];
 }
