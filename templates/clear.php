e377e01a01824bb0be53bf0185de8965fedd1c67
Matt Beall <mbeall@starverte.com>
2013-11-05 15:14:47 -0700

    Add comments back to Clear

--- templates/clear.php
+++ templates/clear.php
@@ -36,6 +36,11 @@ get_header('head'); ?>
           <?php endif; ?>
           
         </article><!-- #page-<?php the_ID(); ?> -->
+        
+        <?php
+          // If comments are open or we have at least one comment, load up the comment template
+          if ( comments_open() || '0' != get_comments_number() )
+            comments_template(); ?>
 
       <?php endwhile; // end of the loop. ?>
 
