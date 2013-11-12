23249cfaa2af453e42aa1e91a9fb38b5dc6b36a2
Matt Beall <mbeall@starverte.com>
2013-11-11 23:40:19 -0700

    Update clear template to reflect theme options

--- templates/clear.php
+++ templates/clear.php
@@ -5,37 +5,18 @@
  * @package Flint
  * @since 1.1.0
  */
-get_header('head'); ?>
+$options = get_option( 'flint_templates' );
+get_header('head');
+if ($options['clear_nav'] == 'navbar'){ get_header('nav'); } ?>
 
   <div id="primary" class="content-area container">
     <div id="content" class="site-content" role="main">
 
       <?php while ( have_posts() ) : the_post(); ?>
       
-        <?php flint_breadcrumbs(); ?>
+        <?php flint_breadcrumbs('clear'); ?>
         
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        
-          <header class="entry-header">
-            <h1 class="entry-title"><?php if (is_singular()) { echo the_title(); } else { $permalink = get_permalink(); $title = get_the_title(); echo '<a href="' . $permalink .'" rel="bookmark">' . $title . '</a>'; } ?></h1>
-          </header><!-- .entry-header -->
-          
-          <?php if ( is_search() ) : // Only display Excerpts for Search ?>
-          <div class="entry-summary">
-            <?php the_excerpt(); ?>
-          </div><!-- .entry-summary -->
-          <?php else : ?>
-          <div class="entry-content">
-            <?php flint_the_content(); ?>
-            <?php
-            flint_link_pages( array(
-              'before' => '<ul class="pagination">',
-              'after'  => '</ul>',
-            ) ); ?>
-          </div><!-- .entry-content -->
-          <?php endif; ?>
-          
-        </article><!-- #page-<?php the_ID(); ?> -->
+        <?php get_template_part( 'templates/' . flint_get_template(), 'content' ); ?>
         
         <?php
           // If comments are open or we have at least one comment, load up the comment template
