8d0f59f382f093a581108863953a1951954119ab
Matt Beall <mbeall@starverte.com>
2013-11-15 09:52:39 -0700

    Update footer.php

--- widgets/footer.php
+++ widgets/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * The "Sidebar" containing the main widget areas.
@@ -17,23 +16,3 @@
       <?php flint_get_widgets_template('margins', 'footer'); ?>
     </div>
   </div><!-- .widgets.widgets-footer -->
-=======
-<?php
-/**
- * The "Sidebar" containing the main widget areas.
- *
- * @package Flint
- */
-?>
-  <div class="canvas-light widgets widgets-footer" role="complementary">
-    <div class="container">
-      <?php flint_get_widgets_template('margins', 'footer'); ?>
-      <div class="widget-area <?php flint_get_widgets_template('content', 'footer'); ?>" >
-        <?php do_action( 'before_sidebar' ); ?>
-        <?php if ( ! dynamic_sidebar( 'footer' ) ) : ?>
-        <?php endif; ?>
-      </div><!-- .widget-area -->
-      <?php flint_get_widgets_template('margins', 'footer'); ?>
-    </div>
-  </div><!-- .widgets.widgets-footer -->
->>>>>>> master
