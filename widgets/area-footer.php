e6aa483a0bc06744b02ada42f9fc7c1bcd38b9b1
Matt Beall <mbeall@starverte.com>
2013-11-26 10:39:00 -0700

    Only show footer widget area if there are widgets

--- widgets/area-footer.php
+++ widgets/area-footer.php
@@ -6,14 +6,15 @@
  * @since 1.1.0
  */
 ?>
+<?php if ( is_active_sidebar( 'footer' ) ) { ?>
   <div class="canvas-light widgets widgets-footer" role="complementary">
     <div class="container">
       <?php flint_get_widgets_template('margins', 'footer'); ?>
       <div class="widget-area <?php flint_get_widgets_template('content', 'footer'); ?>" >
         <?php do_action( 'before_sidebar' ); ?>
-        <?php if ( ! dynamic_sidebar( 'footer' ) ) : ?>
-        <?php endif; ?>
+        <?php dynamic_sidebar('footer'); ?>
       </div><!-- .widget-area -->
       <?php flint_get_widgets_template('margins', 'footer'); ?>
     </div>
   </div><!-- .widgets.widgets-footer -->
+<?php } ?>
