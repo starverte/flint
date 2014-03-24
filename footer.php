4febd34e7345eafafdbda68743ac9782b9505be5
Matt Beall <mbeall@starverte.com>
2014-03-24 00:04:02 -0600

    Slight changes to footer

--- footer.php
+++ footer.php
@@ -13,12 +13,13 @@
 
 <footer id="colophon" class="canvas site-footer" role="contentinfo">
   <div class="site-info container">
-    <div id="custom-footer" class="col-lg-8 col-md-7">
+    <div id="custom-footer" class="col-lg-6 col-md-6">
       <?php flint_custom_footer(); ?>
     </div>
-    <div id="credits" class="col-lg-4 col-md-5">
+    <div id="credits" class="col-lg-6 col-md-6">
       <?php $theme = wp_get_theme(); ?>
-      Proudly powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a> | Theme: <a href="<?php echo $theme->get( 'ThemeURI' ) ?>"><?php echo $theme ?></a> by <?php echo $theme->get( 'Author' ) ?>
+      Proudly powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a><br />
+      Theme: <a href="<?php echo $theme->get( 'ThemeURI' ) ?>"><?php echo $theme ?></a> by <?php echo $theme->get( 'Author' ) ?>
     </div>
   </div><!-- .site-info -->
 </footer><!-- #colophon -->
