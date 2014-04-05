18b32950372ba5e07d1b266a71b65cee0bd2b502
Matt Beall <mbeall@starverte.com>
2014-04-05 15:28:22 -0600

    Add theme support for HTML5

--- functions.php
+++ functions.php
@@ -33,6 +33,8 @@ function flint_after_setup_theme() {
   add_theme_support( 'automatic-feed-links' );
 
   add_theme_support( 'post-thumbnails' );
+	
+	add_theme_support( 'html5' );
 
   register_nav_menus( array(
     'primary' => __( 'Primary Menu', 'flint' ),
