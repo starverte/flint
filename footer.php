569a3d55c83a789526a1410295b9d4db9041f0d4
Matt Beall <mbeall@starverte.com>
2014-10-01 14:49:38 -0600

    Correct indentations/spacing

--- footer.php
+++ footer.php
@@ -14,15 +14,15 @@
 <footer id="colophon" class="canvas site-footer" role="contentinfo">
   <div class="site-info container">
     <div class="row">
-    <div id="custom-footer" class="col-xs-12 col-md-6">
-      <?php flint_custom_footer(); ?>
-    </div>
-    <div id="credits" class="col-xs-12 col-md-6">
-      <?php $theme = wp_get_theme(); ?>
-      Proudly powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a><br />
-      Theme: <a href="<?php echo $theme->get( 'ThemeURI' ) ?>"><?php echo $theme ?></a> by <?php echo $theme->get( 'Author' ) ?>
-    </div>
-  </div><!-- .site-info -->
+      <div id="custom-footer" class="col-xs-12 col-md-6">
+        <?php flint_custom_footer(); ?>
+      </div>
+      <div id="credits" class="col-xs-12 col-md-6">
+        <?php $theme = wp_get_theme(); ?>
+        Proudly powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a><br />
+        Theme: <a href="<?php echo $theme->get( 'ThemeURI' ) ?>"><?php echo $theme ?></a> by <?php echo $theme->get( 'Author' ) ?>
+      </div>
+    </div><!-- .site-info -->
   </div><!-- .row -->
 </footer><!-- #colophon -->
 
