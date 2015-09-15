dea02cd17b8fea3e98c9e8108397c53e6597a633
Matt Beall <mbeall@starverte.com>
2015-09-14 21:05:38 -0600

    Remove instances of `flint_get_sidebar_template()`

--- widgets/area-header.php
+++ widgets/area-header.php
@@ -12,13 +12,13 @@ $type = get_post_type();
 <?php if ( is_active_sidebar( 'header' ) ) { ?>
   <div class="fill-light widgets widgets-header" role="complementary">
     <div class="container">
-      <?php flint_get_sidebar_template( 'margins', 'header' ); ?>
-      <div class="widget-area <?php flint_get_sidebar_template( 'content', 'header' ); ?>" >
+      <?php echo flint_post_margin(); ?>
+      <div class="widget-area <?php echo flint_post_width_class(); ?>" >
         <?php do_action( 'before_sidebar' ); ?>
         <?php do_action( 'flint_widget_area_header_'.$type ); ?>
         <?php dynamic_sidebar( 'header' ); ?>
       </div><!-- .widget-area -->
-      <?php flint_get_sidebar_template( 'margins', 'header' ); ?>
+      <?php echo flint_post_margin(); ?>
     </div>
   </div><!-- .widgets.widgets-header -->
 <?php } ?>
