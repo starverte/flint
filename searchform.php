801f12d601dac228ccc15bb7db131b4e569373eb
Matt Beall <mbeall@starverte.com>
2015-09-17 12:55:05 -0600

    esc_*_ex are not valid functions

--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
 
 ?>
   <form method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-    <label for="s" class="screen-reader-text"><?php esc_html_ex( 'Search', 'assistive text', 'flint' ); ?></label>
-    <input type="search" class="form-control" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'flint' ); ?>" />
+    <label for="s" class="screen-reader-text"><?php esc_html_e( 'Search', 'flint' ); ?></label>
+    <input type="search" class="form-control" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_e( 'Search &hellip;', 'flint' ); ?>" />
   </form>
