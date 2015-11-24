96cdc39f87a8cd1b6d74b54935782e77e81c0213
Matt Beall <mbeall@starverte.com>
2015-11-24 13:07:51 -0700

    Upgrade to Bootstrap 3.3.6

--- functions.php
+++ functions.php
@@ -149,8 +149,8 @@ function flint_enqueue_scripts() {
   /**
    * Load Twitter Bootstrap
    */
-  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.0.0', true );
-  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array() , '3.0.0' );
+  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
+  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array() , '3.3.6' );
 
   wp_enqueue_script( 'flint-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '9f3e2cd', true );
 
