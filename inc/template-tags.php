42735ab467d3069ed531facc30b69890b325e21f
Matt Beall <mbeall@starverte.com>
2016-04-19 16:22:24 -0600

    New file: inc/filters.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -393,19 +393,6 @@ function flint_get_the_content( $more_link_text = 'Read more', $strip_teaser = f
 }
 
 /**
- * Modifies password form to use bootstrap styles
- *
- * @since 1.0.1
- */
-function flint_password_form() {
-  global $post;
-  $label = 'pwbox-' . ( empty( $post->ID ) ? rand() : $post->ID );
-  $o = __( 'To view this protected post, enter the password below:', 'flint' ) . '<form action="' . esc_url( site_url( 'wp-login.php?action=postpass', 'login_post' ) ) . '" method="post" class="form-inline"><input class="form-control pw ' . $label . '" name="post_password" type="password" placeholder="Password"><button class="btn btn-default" type="submit" name="Submit">Submit</button></form>';
-  return $o;
-}
-add_filter( 'the_password_form', 'flint_password_form' );
-
-/**
  * Output a complete commenting form for use within a template, using Twitter Bootstrap styles.
  *
  * @since 1.0.1
