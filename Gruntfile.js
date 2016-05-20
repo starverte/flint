bd85a51b930400f5b7a46d3e030ff56e254187a0
Matt Beall <mbeall@starverte.com>
2016-05-19 21:04:34 -0600

    Simply Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,34 +2,19 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     shell: {
-      empty_tests: {
-        command: 'rm -rfv tests'
-      },
-      syntax_clone: {
-        command: 'git clone https://gist.github.com/e2ab7e46b53e8882ba8e.git tests && rm -rfv tests/.git*'
-      },
-      phpcs_clone: {
-        command: 'git clone https://github.com/squizlabs/PHP_CodeSniffer.git tests/php-codesniffer'
-      },
-      wpcs_clone: {
-        command: 'git clone https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git tests/wordpress-coding-standards'
-      },
       phpcs_config: {
-        command: 'tests/php-codesniffer/scripts/phpcs --config-set installed_paths ../wordpress-coding-standards'
-      },
-      reset_tests: {
-        command: 'echo "" > tests/results'
+        command: 'vendor/bin/phpcs --config-set installed_paths ../../wp-coding-standards/wpcs'
       },
       syntax_tests: {
-        command: "bash tests/syntax.sh >> tests/results"
+        command: "find . -name '*.php' -not -path './node_modules/*' -not -path './tests/*' -not -path './vendor/*' -exec php -lf '{}' \\;"
       },
       phpcs_tests: {
-        command: 'tests/php-codesniffer/scripts/phpcs -p -s -v -n . --standard=./.phpcs.rules.xml --extensions=php --ignore=tests/*,node_modules/* >> tests/results'
+        command: 'vendor/bin/phpcs -p -s -v -n . --standard=./.phpcs.rules.xml --extensions=php --ignore=tests/*,node_modules/*,vendor/*'
       }
     }
   });
 
   grunt.loadNpmTasks( 'grunt-shell' );
-  grunt.registerTask( 'init', ['shell:empty_tests', 'shell:syntax_clone', 'shell:phpcs_clone', 'shell:wpcs_clone', 'shell:phpcs_config'] );
-  grunt.registerTask( 'test', ['shell:reset_tests', 'shell:syntax_tests', 'shell:phpcs_tests'] );
+  grunt.registerTask( 'init', ['shell:phpcs_config'] );
+  grunt.registerTask( 'test', ['shell:syntax_tests', 'shell:phpcs_tests'] );
 }
